c10ed9b14a775e80ffe0456a81cda5e9ac32714e
njuro <juraj.noge@gmail.com>
2018-07-05 21:31:11 +0200

    Make constants static (Lombok bug)

--- src/main/java/com/github/njuro/helpers/Constants.java
+++ src/main/java/com/github/njuro/helpers/Constants.java
@@ -12,19 +12,19 @@ public class Constants {
     /**
      * User content location
      **/
-    public final Path USER_CONTENT_PATH = Paths.get(System.getProperty("user.dir"), "usercontent");
-    public final String USER_CONTENT_URL = "/usercontent/";
+    public static final Path USER_CONTENT_PATH = Paths.get(System.getProperty("user.dir"), "usercontent");
+    public static final String USER_CONTENT_URL = "/usercontent/";
 
     /**
      * Images
      **/
-    public final double IMAGE_MAX_THUMB_WIDTH = 300;
-    public final double IMAGE_MAX_THUMB_HEIGHT = 300;
+    public static final double IMAGE_MAX_THUMB_WIDTH = 300;
+    public static final double IMAGE_MAX_THUMB_HEIGHT = 300;
 
     /**
      * Tripcodes
      */
-    public final String TRIPCODE_SEPARATOR = "!";
-    public final int TRIPCODE_LENGTH = 10;
+    public static final String TRIPCODE_SEPARATOR = "!";
+    public static final int TRIPCODE_LENGTH = 10;
 
 }
