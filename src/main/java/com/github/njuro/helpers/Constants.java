c40cfce62971cbc1e509567fc76def48e38f9312
njuro <juraj.noge@gmail.com>
2018-07-05 19:33:07 +0200

    Mark Constants as utility class

--- src/main/java/com/github/njuro/helpers/Constants.java
+++ src/main/java/com/github/njuro/helpers/Constants.java
@@ -1,28 +1,30 @@
 package com.github.njuro.helpers;
 
+import lombok.experimental.UtilityClass;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public final class Constants {
+
+@UtilityClass
+public class Constants {
 
     /**
      * User content location
      **/
-    public static final Path USER_CONTENT_PATH = Paths.get(System.getProperty("user.dir"), "usercontent");
-    public static final String USER_CONTENT_URL = "/usercontent/";
+    public final Path USER_CONTENT_PATH = Paths.get(System.getProperty("user.dir"), "usercontent");
+    public final String USER_CONTENT_URL = "/usercontent/";
 
     /**
      * Images
      **/
-    public static final double IMAGE_MAX_THUMB_WIDTH = 300;
-    public static final double IMAGE_MAX_THUMB_HEIGHT = 300;
+    public final double IMAGE_MAX_THUMB_WIDTH = 300;
+    public final double IMAGE_MAX_THUMB_HEIGHT = 300;
 
     /**
      * Tripcodes
      */
-    public static final String TRIPCODE_SEPARATOR = "!";
-    public static final int TRIPCODE_LENGTH = 10;
+    public final String TRIPCODE_SEPARATOR = "!";
+    public final int TRIPCODE_LENGTH = 10;
 
-    private Constants() {
-    }
 }
