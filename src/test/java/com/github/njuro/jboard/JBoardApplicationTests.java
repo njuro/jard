26a22594b6393030cf0d01e3daa0fa2e2eedc7df
njuro <juraj.noge@gmail.com>
2018-07-21 21:29:17 +0200

    Setup JUnit 5

---
+++ src/test/java/com/github/njuro/jboard/JBoardApplicationTests.java
@@ -0,0 +1,18 @@
+package com.github.njuro.jboard;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@Disabled
+public class JBoardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
