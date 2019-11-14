8767b18955deb478f1bf514e59d0e3f00a1240a0
njuro <juraj.noge@gmail.com>
2019-11-14 23:52:33 +0100

    Change format of validation errors

--- src/main/java/com/github/njuro/jboard/controllers/rest/validation/ValidationErrors.java
+++ src/main/java/com/github/njuro/jboard/controllers/rest/validation/ValidationErrors.java
@@ -3,22 +3,20 @@ package com.github.njuro.jboard.controllers.rest.validation;
 import lombok.Data;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 
 import java.time.LocalDateTime;
-import java.util.Map;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Data
 public class ValidationErrors {
 
     private LocalDateTime timestamp;
-    private Map<String, String> errors;
+    private List<String> errors;
 
     public ValidationErrors(BindingResult validationResult) {
-        //noinspection ConstantConditions
-        this.errors = validationResult.getFieldErrors().stream().collect(
-                Collectors.toMap(FieldError::getField, DefaultMessageSourceResolvable::getDefaultMessage));
+        this.errors = validationResult.getFieldErrors().stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage).collect(Collectors.toList());
         this.timestamp = LocalDateTime.now();
     }
 }
