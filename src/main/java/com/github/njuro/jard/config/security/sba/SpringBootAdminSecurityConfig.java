0ee6755c5797aa6261fab34d3af985bccb2b1b85
njuro <juraj.noge@gmail.com>
2020-07-16 18:57:13 +0200

    Change condition of SBA autowiring

--- src/main/java/com/github/njuro/jard/config/security/sba/SpringBootAdminSecurityConfig.java
+++ src/main/java/com/github/njuro/jard/config/security/sba/SpringBootAdminSecurityConfig.java
@@ -7,7 +7,7 @@ import de.codecentric.boot.admin.client.registration.BlockingRegistrationClient;
 import de.codecentric.boot.admin.server.web.client.HttpHeadersProvider;
 import java.util.Collections;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,7 +17,10 @@ import org.springframework.web.client.RestTemplate;
 
 /** Configuration of authentication between Spring Boot Admin server and client instances. */
 @Configuration
-@ConditionalOnBean(ClientProperties.class)
+@ConditionalOnProperty(
+    name = "spring.boot.admin.client.enabled",
+    havingValue = "true",
+    matchIfMissing = true)
 public class SpringBootAdminSecurityConfig {
 
   /** Properties of Spring Boot Admin client. */
