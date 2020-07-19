13e7ce075e983287567b7e4711d600378a408a18
njuro <juraj.noge@gmail.com>
2020-07-19 17:08:56 +0200

    Disable HTTPS redirect

--- src/main/java/com/github/njuro/jard/config/security/SecurityConfig.java
+++ src/main/java/com/github/njuro/jard/config/security/SecurityConfig.java
@@ -109,11 +109,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.requiresChannel()
-        .requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
-        .requiresSecure()
-        .and()
-        .authorizeRequests()
+    http.authorizeRequests()
         .antMatchers(springBootAdminProperties.getContextPath() + "/**")
         .hasAuthority(UserAuthority.ACTUATOR_ACCESS.name())
         .requestMatchers(EndpointRequest.toAnyEndpoint())
