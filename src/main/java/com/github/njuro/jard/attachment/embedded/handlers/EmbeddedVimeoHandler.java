c556d87a52739e7af7f470cba3293e2301d79490
njuro <juraj.noge@gmail.com>
2020-07-26 16:39:26 +0200

    Embedded Vimeo handler

---
+++ src/main/java/com/github/njuro/jard/attachment/embedded/handlers/EmbeddedVimeoHandler.java
@@ -0,0 +1,36 @@
+package com.github.njuro.jard.attachment.embedded.handlers;
+
+import ac.simons.oembed.OembedEndpoint;
+import ac.simons.oembed.OembedResponse;
+import com.github.njuro.jard.attachment.Attachment;
+import java.util.Arrays;
+import org.springframework.stereotype.Component;
+
+/** Handler for embedding videos from Vimeo. */
+@Component
+public class EmbeddedVimeoHandler implements EmbeddedAttachmentHandler {
+
+  @Override
+  public String getProviderName() {
+    return "Vimeo";
+  }
+
+  @Override
+  public OembedEndpoint registerEndpoint() {
+    var oembed = new OembedEndpoint();
+    oembed.setName(getProviderName());
+    oembed.setFormat(OembedResponse.Format.json);
+    oembed.setEndpoint("https://vimeo.com/api/oembed.json?autoplay=true&dnt=true");
+    oembed.setUrlSchemes(Arrays.asList("https?://(?:www\\.)?vimeo\\.com/.+"));
+    return oembed;
+  }
+
+  @Override
+  public void setEmbedData(OembedResponse oembedResponse, String embedUrl, Attachment attachment) {
+    if (oembedResponse.getAuthorName() == null) {
+      oembedResponse.setAuthorName("Private Video");
+    }
+
+    EmbeddedAttachmentHandler.super.setEmbedData(oembedResponse, embedUrl, attachment);
+  }
+}
