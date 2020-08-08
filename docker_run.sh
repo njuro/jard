be14cf045afe64387e063532673ea7d74a9e3571
njuro <juraj.noge@gmail.com>
2020-08-08 14:25:08 +0200

    Specify docker container restart policy

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # script for running jard server as docker container
 # don't forget to set enviroment variables in .env file first
-docker run -itd --network host --env-file .env --mount 'type=volume,src=jard-usercontent,dst=/home/cnb/jard-usercontent' --name jard-server njuro/jard-server:latest &&
+docker run -itd --network host --restart unless-stopped --env-file .env --mount 'type=volume,src=jard-usercontent,dst=/home/cnb/jard-usercontent' --name jard-server njuro/jard-server:latest &&
   docker exec -d -u 0 jard-server chown cnb -R /home/cnb/jard-usercontent
