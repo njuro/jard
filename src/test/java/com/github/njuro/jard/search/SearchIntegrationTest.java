12b5658c4141adbad799a86535e1b2264231eef0
njuro <juraj.noge@gmail.com>
2020-08-21 09:43:15 +0200

    Remove unnecessary test

--- src/test/java/com/github/njuro/jard/search/SearchIntegrationTest.java
+++ src/test/java/com/github/njuro/jard/search/SearchIntegrationTest.java
@@ -6,37 +6,25 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.github.njuro.jard.common.Constants;
 import com.github.njuro.jard.common.Mappings;
 import com.github.njuro.jard.common.MockRequestTest;
-import com.github.njuro.jard.common.WithMockUserAuthorities;
 import com.github.njuro.jard.database.UseMockDatabase;
 import com.github.njuro.jard.post.dto.PostDto;
 import com.github.njuro.jard.search.dto.SearchResultsDto;
-import com.github.njuro.jard.user.UserAuthority;
 import org.assertj.core.api.InstanceOfAssertFactories;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpMethod;
-import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest
 @UseMockDatabase
-public class SearchIntegrationTest extends MockRequestTest {
+class SearchIntegrationTest extends MockRequestTest {
 
   @Autowired private SearchFacade searchFacade;
 
   private static final String API_ROOT = Mappings.API_ROOT_SEARCH;
 
   @Test
-  @WithMockUserAuthorities(UserAuthority.ACTUATOR_ACCESS)
-  @DirtiesContext
-  public void testRebuildIndexes() throws Exception {
-    performMockRequest(HttpMethod.GET, API_ROOT + "/rebuild-indexes")
-        .andExpect(status().isOk())
-        .andExpect(nonEmptyBody());
-  }
-
-  @Test
-  public void testSearchPosts() throws Exception {
+  void testSearchPosts() throws Exception {
     searchFacade.rebuildIndexes();
 
     String query = "First";
