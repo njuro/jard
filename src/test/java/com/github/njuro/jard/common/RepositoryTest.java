9e1ca2c30199941cf19fdcbc01641beb0b2e0181
njuro <juraj.noge@gmail.com>
2021-06-23 15:11:18 +0200

    Remove legacy java tests

--- src/test/java/com/github/njuro/jard/common/RepositoryTest.java
+++
@@ -1,8 +0,0 @@
-package com.github.njuro.jard.common;
-
-import com.github.njuro.jard.database.UseMockDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
-@DataJpaTest
-@UseMockDatabase
-public abstract class RepositoryTest {}
